734260225e005648a0ae6e4230157a0a29ef9d85
Luke Menard <lukemenard@gmail.com>
2019-03-21 21:34:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,6 +18,13 @@ function whileLoop(number) {
   return 'done'
 }
 
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
 function doWhileLoop(integer){
   do {
     console.log("I run once regardless.");
