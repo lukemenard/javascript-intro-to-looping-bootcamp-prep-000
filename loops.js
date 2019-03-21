d483ed2d47643e49e1716b42e41b875ad33bdb19
Luke Menard <lukemenard@gmail.com>
2019-03-21 21:13:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,8 @@ function forLoop(array){
     if (i === 1) {
       console.log("I am " + i + " strange loop.")
     }
-    else
-    
+    else {
+      console.log("I am" + ${i} + " strange loops.")
+    }
   }
 }
